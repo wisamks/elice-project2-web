2033e08cd9aabc570d2110c29098961c54ad8d54
[AI11] 송가은 <lucy5591@naver.com>
2024-07-28 21:14:08 +0900

    feat: 비로그인시 헤더 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
+import UnauthenticatedHeader from "./components/commen/UnauthenticatedHeader";
+import AuthenticateHeader from "./components/commen/AuthenticatedHeader";
 import Footer from "./components/commen/Footer";
 
 import Home from './pages/home/Home';
@@ -11,6 +13,7 @@ function App() {
   return (
     <RecoilRoot>
       <Router>
+        <UnauthenticatedHeader />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
