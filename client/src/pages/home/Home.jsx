7971b2f0c93b6fd83e2319d94562e34b33106699
[AI11] 김진미 <jinmi82@naver.com>
2024-07-30 21:08:43 +0900

    chore: 구글 전화번호 수집 삭제

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,16 @@
+import Cookies from 'js-cookie';
+
+const handleLogout = () => {
+    Cookies.remove('access_token');
+    window.location.href = '/sign-in';
+};
+
 const Home = () => {
     return(
-        <div>인덱스</div>
+        <div>
+            인덱스
+            <p onClick={handleLogout}>로그아웃</p>
+        </div>
     );
 };
 
