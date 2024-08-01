220514869f93f9a8b961b6260bce1445e4efc22a
[AI11]김재영A <goodikjy@naver.com>
2024-08-01 10:23:48 +0900

    comment: 안 쓰는 코드 주석화

--- server/src/middlewares/validateNickname.ts
+++ server/src/middlewares/validateNickname.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { nicknameSchema } from '../schemas/nicknameSchema';
-import { findExistNickname } from '../models/userModel';
+// import { findExistNickname } from '../models/userModel';
 
 export async function validateNickname(req: Request, res: Response, next: NextFunction) {
     // Joi 스키마 유효성 검사
@@ -10,10 +10,10 @@ export async function validateNickname(req: Request, res: Response, next: NextFu
     }
 
     // 닉네임 중복 검사
-    const nicknameExists = await findExistNickname(req.body.nickname);
-    if (nicknameExists) {
-        return res.status(409).json({ message: 'Nickname already exists' });
-    }
+    // const nicknameExists = await findExistNickname(req.body.nickname);
+    // if (nicknameExists) {
+    //     return res.status(409).json({ message: 'Nickname already exists' });
+    // }
 
     next();
 } 
--- server/src/schemas/nicknameSchema.ts
+++ server/src/schemas/nicknameSchema.ts
@@ -1,8 +1,8 @@
-import Joi from 'joi';
+// import Joi from 'joi';
 
-export const nicknameSchema = Joi.object({
-    nickname: Joi.string().min(1).max(40).required()
-});
+// export const nicknameSchema = Joi.object({
+//     nickname: Joi.string().min(1).max(40).required()
+// });
 
 
 import Joi from 'joi';
