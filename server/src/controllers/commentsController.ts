09a086388badcbe196f05f678cea851097ccc54f
[AI11]김재영A <goodikjy@naver.com>
2024-08-04 14:01:56 +0900

    comment: 댓글 api 작업 주석 추가

--- server/src/controllers/commentsController.ts
+++ server/src/controllers/commentsController.ts
@@ -3,19 +3,20 @@ import { Request, Response, NextFunction } from 'express';
 class CommentsController {
     // 게시글에서 댓글 조회
     static async getCommentsByPostId(req: Request, res: Response, next: NextFunction) {
-
+        // 페이지네이션 적용, 유저 테이블 조인
+        // 전체 댓글 개수 이미 있긴 한데... 혹시 모르니...
     }
     // 댓글 생성
     static async createComment(req: Request, res: Response, next: NextFunction) {
-
+        // 그냥 생성
     }
     // 댓글 수정
     static async updateComment(req: Request, res: Response, next: NextFunction) {
-
+        // 있는지 확인하고 404, 유저 아이디 다르면 403, 그 다음 수정
     }
     // 댓글 삭제
     static async deleteComment(req: Request, res: Response, next: NextFunction) {
-
+        // 있는지 확인하고 404, 유저 아이디 다르면 403, 그 다음 삭제
     }
 }
 
--- server/src/routers/commentsRouter.ts
+++ server/src/routers/commentsRouter.ts
@@ -1,11 +1,13 @@
-import CommentsController from '@_/controllers/commentsController';
 import { Router } from 'express';
+
+import CommentsController from '@_/controllers/commentsController';
+import { authenticateAccessToken } from '@_middlewares';
 
 const router = Router();
 
 router.get('/', CommentsController.getCommentsByPostId);
-router.post('/', CommentsController.createComment);
-router.put('/:commentId', CommentsController.updateComment);
-router.delete('/:commentId', CommentsController.deleteComment);
+router.post('/', authenticateAccessToken, CommentsController.createComment);
+router.put('/:commentId', authenticateAccessToken, CommentsController.updateComment);
+router.delete('/:commentId', authenticateAccessToken, CommentsController.deleteComment);
 
 export default router;
