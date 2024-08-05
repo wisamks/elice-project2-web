49772fdc851888ea3a3ce75b7d3a7a7568e1ff3c
[AI11] 송가은 <lucy5591@naver.com>
2024-08-06 01:52:09 +0900

    feat: 댓글 CRUD

---
+++ client/src/components/board/comment/comment.js
@@ -0,0 +1,77 @@
+// 조회
+export const fetchComments = async (postId, page = 1, perPage = 10) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/comments/?postId=${postId}&page=${page}&perPage=${perPage}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch comments');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error fetching comments:', error);
+      throw error;
+    }
+};
+  
+  // 생성
+export const createComment = async (postId, content, secret) => {
+  try {
+    const response = await fetch('http://localhost:8080/api/comments/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ postId, content, secret }),
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to create comment');
+    }
+    
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error creating comment:', error);
+    throw error;
+  }
+};
+  
+// 수정
+export const updateComment = async (commentId, content) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/comments/${commentId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content }),
+      credentials: 'include',
+    });
+
+    if (response.ok) {
+        return true;
+    } else {
+        throw new Error('Failed to update comment');
+    }
+  } catch (error) {
+    console.error('Error updating comment:', error);
+    throw error;
+  }
+};
+  
+// 삭제
+export const deleteComment = async (commentId) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/comments/${commentId}`, {
+      method: 'DELETE',
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete comment');
+    }
+    return true;
+  } catch (error) {
+    console.error('Error deleting comment:', error);
+    throw error;
+  }
+};
+  
