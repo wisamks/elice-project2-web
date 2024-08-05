01e347be35bb4d369523f388b8c8f1b0d3d9a29a
[AI11]김재영A <goodikjy@naver.com>
2024-08-05 13:45:15 +0900

    refactor: 이미지 타입 추가

--- server/src/controllers/imagesController.ts
+++ server/src/controllers/imagesController.ts
@@ -1,10 +1,15 @@
-import {Request, Response, NextFunction} from 'express';
+import { Image } from '@_/customTypes/imagesType';
+import { InternalServerError } from '@_/utils/customError';
+import { Request, Response, NextFunction } from 'express';
 
 class ImagesController {
     static async putImages(req: Request, res: Response, next: NextFunction) {
         try {
-            const files = req.files as any;
-            const urls = files.images.map(image => image.location);
+            const files = req.files as {images: Array<Image>}|undefined;
+            if (files === undefined) {
+                throw new InternalServerError('이미지 url을 생성하는 데 실패했습니다.');
+            }
+            const urls = files.images.map((image: Image) => image.location);
             res.status(200).json({ urls });
         } catch (err) {
             next(err);
