a656f164770ee0f3cb967bec6ec46fb583dd3051
AI 11 류진형 <jinhyung3012@gmail.com>
2024-08-07 22:30:11 +0000

    hotfix: 진행중..

--- client/src/controllers/baseURI.js
+++ client/src/controllers/baseURI.js
@@ -1,4 +1,2 @@
 // export const baseURI =  'http://localhost:8080';
-export const baseURI = process.env.API_BASE_URL || 'http://localhost:8080';
-
-console.log(baseURI);
+export const baseURI = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
