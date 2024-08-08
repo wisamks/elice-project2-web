8549f30e948b6dd8058d1566acfb5cfd4ad734d8
[AI11]김재영A <goodikjy@naver.com>
2024-08-08 17:34:55 +0900

    comment: validator 주석 처리 작업

--- server/src/middlewares/schemas/commentSchema.ts
+++ server/src/middlewares/schemas/commentSchema.ts
@@ -1,11 +1,12 @@
-// src/middlewares/schemas/commentSchema.ts
-import Joi from 'joi';
+// DTO로 이사갔습니다.
 
-export const createCommentSchema = Joi.object({
-    postId: Joi.number().integer().required(),
-    content: Joi.string().min(1).max(300).required()
-});
+// import Joi from 'joi';
 
-export const updateCommentSchema = Joi.object({
-    content: Joi.string().min(1).max(300).required()
-});
+// export const createCommentSchema = Joi.object({
+//     postId: Joi.number().integer().required(),
+//     content: Joi.string().min(1).max(300).required()
+// });
+
+// export const updateCommentSchema = Joi.object({
+//     content: Joi.string().min(1).max(300).required()
+// });
