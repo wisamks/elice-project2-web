d457e9d4def0eb902e45ffc127f16571830c4d52
[AI11]김재영A <goodikjy@naver.com>
2024-08-08 16:59:56 +0900

    fix: 가격대 오류 수정

--- server/src/utils/calculatePriceRange.ts
+++ server/src/utils/calculatePriceRange.ts
@@ -2,17 +2,17 @@ const calculatePriceRange = (price: number|undefined) => {
     if (price === undefined) {
         return undefined;
     }
-    if (price > 50000) {
+    if (price >= 50000) {
         return {min: 50000, max: undefined};
-    } else if (price > 30000) {
+    } else if (price >= 30000) {
         return {min: 30000, max: 50000}; 
-    } else if (price > 10000) {
+    } else if (price >= 10000) {
         return {min: 10000, max: 30000};
-    } else if (price > 5000) {
+    } else if (price >= 5000) {
         return {min: 5000, max: 10000};
-    } else if (price > 1000) {
+    } else if (price >= 1000) {
         return {min: 1000, max: 5000};
-    } else if (price > 0) {
+    } else if (price >= 1) {
         return {min: 1, max: 1000};
     } else {
         return {min: undefined, max: 0};
